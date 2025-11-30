33b45842eceaa31b15909a13be1a1348b66ab9f5
Шеина Татьяна <tsheina@yandex.ru>
2025-11-30 15:24:37 +0500

    init

---
+++ gui_manager.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import shutil
+import os
+import datetime
+
+# Создание основного окна
+root = tk.Tk()
+root.title("Файловый менеджер")
+root.geometry("600x600")
+root.resizable(width=False, height=False)
+root.configure(background="white")
+
+
+# Запуск основного цикла обработки событий
+root.mainloop()
